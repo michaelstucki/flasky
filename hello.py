746ea4b60003a014f4d22b167da1a2cbf0617d2b
michaelstucki <orange.urn@gmail.com>
2020-04-08 17:10:48 -0600

    templates, bootstrap

--- hello.py
+++ hello.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h2>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('user.html', name=name)
